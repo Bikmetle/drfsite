3f28c8a4d1ccff892b4356b6c70f6d0acfcf820e
Bikmetle <fidmargai@gmail.com>
2022-09-06 16:08:59 +0300

    3

--- women/views.py
+++ women/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Women
 from women.serializer import WomenSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.forms import model_to_dict
 
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        new_post = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(new_post)})
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
