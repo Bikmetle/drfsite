3e38b6efe81f2226e46de2ba371b9eab2897aa34
Bikmetle <fidmargai@gmail.com>
2022-09-12 17:17:32 +0300

    5

--- women/serializer.py
+++ women/serializer.py
@@ -1,20 +1,8 @@
 import io
-
 from rest_framework import serializers
-from .models import Women
-from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-
-# class WomenSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Women
-#         fields = ('title', 'cat_id')
-
-
-# class WomenModel:
-#     def __init__(self, title, content):
-#         self.title = title
-#         self.content = content
+from rest_framework.renderers import JSONRenderer
+from .models import Women
 
 
 class WomenSerializer(serializers.Serializer):
@@ -25,18 +13,17 @@ class WomenSerializer(serializers.Serializer):
     is_published = serializers.BooleanField(default=True)
     cat_id = serializers.IntegerField()
 
+    def create(self, validated_data):
+        return Women.objects.create(**validated_data)
 
-# def encode():
-#     model = WomenModel('Angelina', 'something')
-#     model_sr = WomenSerializer(model)
-#     print(model_sr.data, type(model_sr.data), sep='\n')
-#     json = JSONRenderer().render(model_sr.data)
-#     print(json)
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.time_updated = validated_data.time_updated('time_updated', instance.time_updated)
+        instance.is_published = validated_data.is_published('is_published', instance.is_published)
+        instance.cat_id = validated_data.cat_id('cat_id', instance.cat_id)
+        instance.save()
+        return instance
 
-
-# def decode():
-#     stream = io.BytesIO(b'{"title":"Angelina", "content":"something"}')
-#     data = JSONParser().parse(stream)
-#     serializer = WomenSerializer(data=data)
-#     serializer.is_valid()
-#     print(serializer.validated_data)
+    def delete(self, instance):
+        return Women.objects.delete(instance)
