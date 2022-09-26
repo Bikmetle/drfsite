d25bf759b2df470101e51e81b7ffafc4ddccd35d
Bikmetle <fidmargai@gmail.com>
2022-09-26 20:15:47 +0300

    15

--- women/views.py
+++ women/views.py
@@ -2,6 +2,7 @@ from django.forms import model_to_dict
 from rest_framework import generics, viewsets, mixins
 from django.shortcuts import render
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
@@ -12,11 +13,17 @@ from .serializers import WomenSerializer
 from .permissions import IsOwnerOrReadOnly
 
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+
 class WomenViewSet(viewsets.ModelViewSet):
 #    queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsOwnerOrReadOnly, )
-
+    pagination_class = WomenAPIListPagination
     def get_queryset(self):
         pk = self.kwargs.get('pk')
         if not pk:
